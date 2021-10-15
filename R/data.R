69b4b849b6f2ca1b8e678b1668989175cecdba3a
Athanasia Monika Mowinckel <a.m.mowinckel@psykologi.uio.no>
2021-10-15 14:49:47 +0200

    init

---
+++ R/data.R
@@ -0,0 +1,27 @@
+#' power atlas
+#'
+#' Add a short description of this atlas.
+#'
+#' @docType data
+#' @name power
+#' @keywords datasets
+#' @family ggseg_atlases ggseg3d_atlases
+#' @references Add reference to the atlas description paper. make sure to add the url to pubmed
+#'     (\href{https://pubmed.ncbi.nlm.nih.gov/xxxxx/}{PubMed})
+#'     
+#' \itemize{
+#'  \item{power}{ - power atlas}
+#'  \item{power_3d}{ - power 3d mesh atlas}
+#'}
+#'
+#' @import ggseg
+#' @import ggseg3d
+#' @rdname power
+#' @examples
+#' data(power)
+#' data(power_3d)
+"power"
+
+#' @rdname power
+"power_3d"
+
